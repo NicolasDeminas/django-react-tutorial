d0b2327252b5a6761bfe89e74985cd894cd8ee6b
Nico <deminas.nicolas@gmail.com>
2021-04-22 10:34:21 -0300

    Se agregar funcion para mantener la sesion abierta y devolver la misma sala

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RoomViews, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomViews, CreateRoomView, GetRoom, JoinRoom, UserInRoom
 
 urlpatterns = [
     path('room', RoomViews.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
-    path('join-room', JoinRoom.as_view())
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view())
+    
 ]
